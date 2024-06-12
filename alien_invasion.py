4fa15c49027891aed0316be6bb3119137307905c
kuma-babylontech <kuma@babylon-tech.com>
2024-06-12 10:25:35 +0900

    宇宙船を画面に描画

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -15,6 +16,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("エイリアン侵略")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """ゲームのメインループを開始する"""
         while True:
@@ -25,6 +28,7 @@ class AlienInvasion:
 
             # ループを通過するたびに画面を再描画する
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # 最新の状態の画面を表示する
             pygame.display.flip()
