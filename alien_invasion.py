fc2b940a53e05f7f25a0b87d3be1249cd3004d5b
kuma-babylontech <kuma@babylon-tech.com>
2024-06-12 10:18:13 +0900

    Settingsクラスを追加

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """ゲームのアセットと動作を管理する全体的なクラス"""
@@ -8,12 +10,10 @@ class AlienInvasion:
     def __init__(self):
         """ゲームを初期化し、ゲームのリソースを作成する"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("エイリアン侵略")
-
-        # 背景色を設定する
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """ゲームのメインループを開始する"""
@@ -24,7 +24,7 @@ class AlienInvasion:
                     sys.exit()
 
             # ループを通過するたびに画面を再描画する
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # 最新の状態の画面を表示する
             pygame.display.flip()
